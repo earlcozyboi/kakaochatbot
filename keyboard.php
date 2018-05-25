69058aa3783b5809fcf72433f12c22c8fc74c465
earlcozyboi <35266382+earlcozyboi@users.noreply.github.com>
2018-05-26 02:26:58 +0900

    Update keyboard.php

--- keyboard.php
+++ keyboard.php
@@ -5,8 +5,3 @@
         "buttons": ["메뉴1", "메뉴2", "메뉴3"]
     }';
 ?> 
-RewriteEngine on
-RewriteCond %{THE_REQUEST} /([^.]+)\.php [NC]
-RewriteRule ^ /%1 [NC,L,R]
-RewriteCond %{REQUEST_FILENAME}.php -f 
-RewriteRule ^(.*?)/?$ /$1.php [NC,L]
